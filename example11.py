2bd7da13b2574c0fcd2044ccf55ca4423a25c351
zoumson <stargue49@gmail.com>
2024-04-10 10:55:30 +0800

    started multivariate lstm integration

--- example11.py
+++ example11.py
@@ -79,7 +79,7 @@ def modelNN1(dataframe):
     print(train_data_x.shape)
     # model_ = Model()
     # inputs = tf.keras.input(shape=(32, 32, 1))
-    inputs = keras.Input(shape=(batch_size, train_data_x.shape[0], ))  # input layer
+    inputs = keras.Input(shape=(batch_size, train_data_x.shape[0],))  # input layer
     x = LSTM(5)(inputs)  # hidden layer
     outputs = Dense(1)(x)  # output layer
     #
@@ -125,6 +125,8 @@ def modelNN1(dataframe):
     # plt.plot(trainPredictPlot)
     # plt.plot(testPredictPlot)
     # plt.show()
+
+
 def modelNN2(df):
     import numpy as np
     import matplotlib.pyplot as plt
@@ -234,6 +236,106 @@ def modelNN2(df):
     # plot the actual price, prediction in test data=red line, actual price=blue line
     # plt.plot(testPredictPlot)
     # plt.show()
+
+
+def modelNN3(dataset):
+
+    from math import sqrt
+    from numpy import concatenate
+    from matplotlib import pyplot
+    # from pandas import read_csv
+    from pandas import DataFrame
+    from pandas import concat
+    from sklearn.preprocessing import MinMaxScaler
+    from sklearn.preprocessing import LabelEncoder
+    from sklearn.metrics import mean_squared_error
+    from keras.models import Sequential
+    from keras.layers import Dense
+    from keras.layers import LSTM
+
+    # convert series to supervised learning
+    def series_to_supervised(data, n_in=1, n_out=1, dropnan=True):
+        n_vars = 1 if type(data) is list else data.shape[1]
+        df = DataFrame(data)
+        cols, names = list(), list()
+        # input sequence (t-n, ... t-1)
+        for i in range(n_in, 0, -1):
+            cols.append(df.shift(i))
+            names += [('var%d(t-%d)' % (j + 1, i)) for j in range(n_vars)]
+        # forecast sequence (t, t+1, ... t+n)
+        for i in range(0, n_out):
+            cols.append(df.shift(-i))
+            if i == 0:
+                names += [('var%d(t)' % (j + 1)) for j in range(n_vars)]
+            else:
+                names += [('var%d(t+%d)' % (j + 1, i)) for j in range(n_vars)]
+        # put it all together
+        agg = concat(cols, axis=1)
+        agg.columns = names
+        # drop rows with NaN values
+        if dropnan:
+            agg.dropna(inplace=True)
+        return agg
+
+    # load dataset
+    # dataset = read_csv('pollution.csv', header=0, index_col=0)
+    values = dataset.values
+    # integer encode direction
+    # encoder = LabelEncoder()
+    # values[:, 4] = encoder.fit_transform(values[:, 4])
+    # ensure all data is float
+    values = values.astype('float32')
+    # normalize features
+    scaler = MinMaxScaler(feature_range=(0, 1))
+    scaled = scaler.fit_transform(values)
+    # frame as supervised learning
+    reframed = series_to_supervised(scaled, 1, 1)
+    # drop columns we don't want to predict
+    reframed.drop(reframed.columns[[6, 7, 8, 9]], axis=1, inplace=True)
+    print(reframed.head())
+    #
+    # # split into train and test sets
+    # values = reframed.values
+    # n_train_hours = 365 * 24
+    # train = values[:n_train_hours, :]
+    # test = values[n_train_hours:, :]
+    # # split into input and outputs
+    # train_X, train_y = train[:, :-1], train[:, -1]
+    # test_X, test_y = test[:, :-1], test[:, -1]
+    # # reshape input to be 3D [samples, timesteps, features]
+    # train_X = train_X.reshape((train_X.shape[0], 1, train_X.shape[1]))
+    # test_X = test_X.reshape((test_X.shape[0], 1, test_X.shape[1]))
+    # print(train_X.shape, train_y.shape, test_X.shape, test_y.shape)
+    #
+    # # design network
+    # model = Sequential()
+    # model.add(LSTM(50, input_shape=(train_X.shape[1], train_X.shape[2])))
+    # model.add(Dense(1))
+    # model.compile(loss='mae', optimizer='adam')
+    # # fit network
+    # history = model.fit(train_X, train_y, epochs=50, batch_size=72, validation_data=(test_X, test_y), verbose=2,
+    #                     shuffle=False)
+    # # plot history
+    # pyplot.plot(history.history['loss'], label='train')
+    # pyplot.plot(history.history['val_loss'], label='test')
+    # pyplot.legend()
+    # pyplot.show()
+    #
+    # # make a prediction
+    # yhat = model.predict(test_X)
+    # test_X = test_X.reshape((test_X.shape[0], test_X.shape[2]))
+    # # invert scaling for forecast
+    # inv_yhat = concatenate((yhat, test_X[:, 1:]), axis=1)
+    # inv_yhat = scaler.inverse_transform(inv_yhat)
+    # inv_yhat = inv_yhat[:, 0]
+    # # invert scaling for actual
+    # test_y = test_y.reshape((len(test_y), 1))
+    # inv_y = concatenate((test_y, test_X[:, 1:]), axis=1)
+    # inv_y = scaler.inverse_transform(inv_y)
+    # inv_y = inv_y[:, 0]
+    # # calculate RMSE
+    # rmse = sqrt(mean_squared_error(inv_y, inv_yhat))
+    # print('Test RMSE: %.3f' % rmse)
 
 
 def set_simple_strategy(_email, _api_secret, _api_key):
@@ -443,7 +545,8 @@ def main():
     # ui.load_data(get_hist)
     data_here = get_hist_yahoo()
     # modelNN1(data_here[['Close']])
-    modelNN2(data_here[['Close']])
+    # modelNN2(data_here[['Close']])
+    modelNN3(data_here)
 
 
 if __name__ == "__main__":
